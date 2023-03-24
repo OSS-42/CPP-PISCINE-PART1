126ce019842d2cc139555fe0a874364ae25b9aa7
OSS-42 <eric.wursteisen@gmail.com>
2023-03-24 12:45:39 -0400

    fix multiple args to megaphone

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: ewurstei <ewurstei@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 09:17:52 by ewurstei          #+#    #+#             */
-/*   Updated: 2023/03/24 11:01:30 by ewurstei         ###   ########.fr       */
+/*   Updated: 2023/03/24 12:45:19 by ewurstei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,27 @@
 
 int	main (int argc, char **argv)
 {
+	int	av;
 	int	i;
 
-	i = -1;
-	if (argc > 2)
-	{
-		std::cout << "usage : ./megaphone \"your message\"" << std::endl;
-		return (0);
-	}
-	else if (argc == 1)
+	av = 0;
+	if (argc == 1)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
-	std::string str (argv[1]);
-	std::size_t size = str.size();
-	while (++i < (int)size)
+	while (argv[++av])
 	{
-		if (str[i] >= 97 && str[i] <= 122 && str[i])
-			str[i] -= 32;
+		std::string str (argv[av]);
+		std::size_t size = str.size();
+		i = -1;
+		while (++i < (int)size)
+		{
+			if (str[i] >= 97 && str[i] <= 122 && str[i])
+				str[i] -= 32;
+		}
+		std::cout << str;
 	}
-	std::cout << str << std::endl;
+	std::cout << std::endl;
 	return (0);
 }
